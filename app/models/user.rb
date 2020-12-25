f72561482c331bd8bbab9f26d5f0abb69db3dda6
rk-tech-camp <rkoska198905@icloud.com>
2020-12-25 14:44:15 +0900

    ログイン、ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :items
+         has_many :orders
+
+
+         
+        #  validates :email,inclusion: { in:["@"] }
+        #  validates :encrypted_password,  length: { minimum: 6 }
+         validates :password,  format: { with:/\A[a-z0-9]+\z/}
+         #validates :, confirmation: true
+         validates :first_name, format: { with: /\A[一-龥]+\z/}
+         validates :last_name, format: { with:/\A[一-龥]+\z/}
+         validates :fname_kana, format: { with: /\p{katakana}/ }
+         validates :lname_kana, format: { with: /\p{katakana}/ }
+         
+         
+
 end
--- db/migrate/20201223150556_devise_create_users.rb
+++ db/migrate/20201223150556_devise_create_users.rb
@@ -4,7 +4,20 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nick_name,      null: false
+      t.string :first_name,     null: false
+      t.string :last_name ,     null: false
+      t.string :fname_kana ,    null: false
+      t.string :lname_kana,     null: false
+      t.date   :birthday,        null: false
+
+
+
+
+
+
+
+      t.string :email,              null: false , unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
