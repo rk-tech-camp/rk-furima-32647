6f89f7f1ad27b9fd60128971e8f225a615da77be
rk-tech-camp <rkoska198905@icloud.com>
2021-01-07 17:39:54 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:edit, :update,:new,:create]
+  before_action :authenticate_user!,except: [:index, :show]
 	
 	before_action :contributor_confirmation, only: [:edit, :update]
 
